18a0b844604b132bd7c145b8638e35c080105af1
Phillip Marashian <pmarashian@bayareanewsgroup.com>
2016-05-27 14:59:51 -0700

    update file types.  define sizes argument

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,47 @@ var gulp        = require( 'gulp' ),
 	parallel    = require( 'concurrent-transform' ),
 	del         = require( 'del' ),
 	pipes       = require( 'gulp-pipes' ),
+	gutil       = require( 'gulp-util' ),
+	minimist    = require( 'minimist' ),
 
+	argv             = minimist( process.argv.slice(2) ),
 	resizeImageTasks = [],
+	sizes            = [],
+	argumentSizes    = [];
 
-	sizes = [ 75, 150, 300, 450 ];
+const chalk = require('chalk');
+
+if( typeof argv.size === 'undefined' ) {
+
+	gutil.log( chalk.bold.red( 'No sizes given.  Use "npm run resize -- --size [int]"' ) );
+
+} else {
+
+	if( typeof argv.size == 'number' ) {
+
+		argumentSizes.push( argv.size );
+
+	} else {
+
+		argumentSizes = argv.size;
+
+	}
+
+	argumentSizes.forEach( function( size ) {
+
+		if( typeof size === 'number' ) {
+
+			sizes.push ( size );
+
+		} else {
+
+			gutil.log( chalk.bold.red( size + ' is not a valid size' ) );
+
+		}
+
+	});
+
+}
 
 function clean() {
 
@@ -23,7 +60,7 @@ sizes.forEach( function( size ) {
 
 		return gulp
 
-			.src( "src/**/*.jpg" )
+			.src( "src/**/*.{jpg,png,tiff}" )
 
 			.pipe( parallel( imageResize({
 				width:  size,
@@ -48,5 +85,13 @@ sizes.forEach( function( size ) {
 
 });
 
-gulp.task( 'resize', gulp.series( clean, gulp.parallel( resizeImageTasks ) ) );
+var resizeTasks = [ clean ];
+
+if( resizeImageTasks.length ) {
+
+	resizeTasks.push( gulp.parallel( resizeImageTasks ) );
+
+}
+
+gulp.task( 'resize', gulp.series( resizeTasks ) );
 gulp.task( 'clean', clean );
